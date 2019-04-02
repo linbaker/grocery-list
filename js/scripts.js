46ae06bb8fadf874e10cbfda72ae177e5ecf438a
Lindsey Baker <lindsey.baker0@gmail.com>
2019-04-01 22:30:16 -0700

    Return inputs of forms in alaphabetical order

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,24 @@
 $(function() {
   $("#grocery-form").submit(function(event) {
     var groceries = ["item1", "item2", "item3", "item4", "item5"];
-    console.log("1", groceries);
 
+
+    var gList = [];
     groceries.forEach(function(grocery) {
-      var groceryInput = $("input#" + grocery).val();
-      $(".groceries").sort().append("<li>" + groceryInput.charAt(0).toUpperCase() + groceryInput.slice(1) + "</li>").val();
+    var groceryInput = $("input#" + grocery).val();
 
-      console.log("2", groceryInput);
-    });
+    gList.push(groceryInput);
+
     event.preventDefault();
+
+    });
+    gList.sort();
+    alert(gList);
+    gList.forEach(function(bob) {
+    $(".groceries").append("<li>" + bob.charAt(0).toUpperCase() + bob.slice(1) + "</li>").val();
+
+    event.preventDefault();
+
+    });
   });
 });
